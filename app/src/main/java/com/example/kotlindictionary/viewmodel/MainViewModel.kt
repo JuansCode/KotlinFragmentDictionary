c68278a453cbf75d304c8c69720dc83d466056e8
jpimentel45 <jpimentel45@gmail.com>
2020-03-23 11:34:04 -0400

    Changed sort to send to data as list

--- app/src/main/java/com/example/kotlindictionary/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/kotlindictionary/viewmodel/MainViewModel.kt
@@ -65,12 +65,12 @@ class MainViewModel: CustomViewModel() {
         if(!clicked){
             clicked = true
             adapter.updateData(
-                data.sortedByDescending { it.thumbs_up }
+                data.sortedWith(compareByDescending{it.thumbs_up}).toList()
             )
         }else{
             clicked = false
             adapter.updateData(
-                data.sortedByDescending { it.thumbs_down }
+                data.sortedWith(compareByDescending{ it.thumbs_down }).toList()
             )
         }
     }
